3accc8da88a20aa5b2bf0b52b43b4b747b1d0b36
William Pannell <lpann5@gmail.com>
2020-04-09 00:03:13 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -1,9 +1,16 @@
 require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
 require_relative './fancy_dance.rb'
-require_relative './dance_party.rb'
 
 class Kid
   extend FancyDance::ClassMethods
   include FancyDance::InstanceMethods
+  
+  include Dance
+ 
+  attr_accessor :name
+ 
+  def initialize(name)
+    @name = name
+  end
 end
