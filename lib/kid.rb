ee041609bccb1aa130bac3a9d3b55b553d102d8a
William Pannell <lpann5@gmail.com>
2020-04-09 00:01:44 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -1,6 +1,7 @@
 require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
 require_relative './fancy_dance.rb'
+require_relative './dance_party.rb'
 
 class Kid
   extend FancyDance::ClassMethods
